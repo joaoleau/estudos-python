045878806a985decb60afd1338b34968d6ec5703
Joao Leal <joaoo.lealc@gmail.com>
2023-05-09 18:30:47 -0300

    Aula 02 04 While

---
+++ src/02-controle-de-fluxo/aula04.py
@@ -0,0 +1,14 @@
+"""Aula 02 04 While"""
+
+# Quer percorrer uma listsa qeu nao ta em memoria completamente
+# while condicao:
+#   instrucao
+
+contador = 0
+while contador <= 10:
+    print(contador)
+    contador += 1 #Contador precisa ser in/decrementado senão entra em infinito
+                
+print('Fim')
+
+ 
