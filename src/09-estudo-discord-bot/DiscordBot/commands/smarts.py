4253f1ef55d0781363fed0317743c57e2479d9de
Joao Leal <joaoo.lealc@gmail.com>
2023-05-27 11:05:37 -0300

    Exercicio 02 DiscordBot

--- src/09-estudo-discord-bot/DiscordBot/commands/smarts.py
+++ src/09-estudo-discord-bot/DiscordBot/commands/smarts.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 class Smarts(commands.Cog):
     """A lot of Smart Commands"""
@@ -13,7 +14,7 @@ class Smarts(commands.Cog):
 
     # Exercicio 01
     @commands.command(name="cadastrar", help="Cadastra aluno. Ex: SP2000 'Marcia Silva' marcia@gmail.com")
-    async def user_register(self, ctx, prontuario, nome, email):
+    async def student_register(self, ctx, prontuario, nome, email):
         with open("src/09-estudo-discord-bot/DiscordBot/alunos.txt", "r") as arquivo:
             arquivo_str = arquivo.read()
         if prontuario in arquivo_str:
@@ -24,6 +25,21 @@ class Smarts(commands.Cog):
             with open("src/09-estudo-discord-bot/DiscordBot/alunos.txt", "a") as arquivo:
                 arquivo.write(f'{str(prontuario)},{str(nome)},{str(email)} \n')
             await ctx.send("Cadastrado com sucesso")
+    
+    # Exercicio 02
+    @commands.command(name="listar-alunos", help="Exibi uma lista de alunos cadastrados")
+    async def students(self, ctx):
+        embed = discord.Embed(
+            title = "Lista de Alunos",
+            color = 0x0000FF
+        )
+        embed.set_author(name=self.bot.user.name, icon_url=self.bot.user.avatar)
+        with open("src/09-estudo-discord-bot/DiscordBot/alunos.txt", "r") as arquivo:
+            i=1
+            for linha in arquivo.readlines():
+                embed.add_field(name=f"{i}º Aluno(a): ",value=linha, inline=False)
+                i+=1
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(Smarts(bot))
